2de4cc9b11070cbaab1a4b187439ce3bad299507
minsol0302 <pjjw1233@gmail.com>
2025-12-05 18:20:09 +0900

    머신러닝

--- ai.minsol.kr/mlservice/app/config.py
+++ ai.minsol.kr/mlservice/app/config.py
@@ -6,11 +6,10 @@ from common.config import BaseServiceConfig
 
 class TitanicServiceConfig(BaseServiceConfig):
     """타이타닉 서비스 설정"""
-    service_name: str = "titanicservice"
+    service_name: str = "mlservice"
     service_version: str = "1.0.0"
-    port: int = 9000
+    port: int = 9010
     
     class Config:
         env_file = ".env"
         case_sensitive = False
-
